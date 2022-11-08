d808bb2e6e29ef9986933d03c751f6541faa76f3
Alina <alina.andrevna3993@gmail.com>
2022-11-08 17:55:43 +0300

    reveiwer comments
    made getIngredient async and renamed const IngredientType

--- src/utils/api-ingredients.js
+++ src/utils/api-ingredients.js
@@ -4,11 +4,10 @@ const checkResponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
 
-export const getIngredients = () => {
-  return fetch("${url}/ingredients")
-    .then(checkResponse)
-    .then((data) => {
-      if (data?.success) return data.data;
-      return Promise.reject(data);
-    });
+export const getIngredients = async () => {
+  const res = await fetch("${url}/ingredients");
+  const data = await checkResponse(res);
+  if (data?.success)
+    return data.data;
+  return Promise.reject(data);
 };
--- src/utils/types.js
+++ src/utils/types.js
@@ -1,7 +1,7 @@
 
 import PropTypes from 'prop-types';
 
-export const burgerTypes = PropTypes.shape({
+export const ingredientType = PropTypes.shape({
     _id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired ,
     type: PropTypes.oneOf(['bun', 'main', 'sause']).isRequired,
