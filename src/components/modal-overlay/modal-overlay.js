5b176c77fbfdddcef41c0976b9ede78fe30dd4ff
Alina <alina.andrevna3993@gmail.com>
2022-11-09 11:48:02 +0300

    overlay fixes
    fixed overlay background

--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -3,7 +3,7 @@ import modalOverlayStyles from "./modal-overlay.module.css";
 import PropTypes from "prop-types";
 
 const ModalOverlay = ({ toggleModal  }) => {
-  return <div className={modalOverlayStyles.modalOverlayStyles} onClick={toggleModal }></div>;
+  return <div className={modalOverlayStyles.modalOverlay} onClick={toggleModal }></div>;
 };
 
 ModalOverlay.propTypes = {
