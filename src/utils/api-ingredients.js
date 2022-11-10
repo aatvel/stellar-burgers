4ddc4223b5e2100b8b566485830fe4e0c4cdf3a5
Alina <alina.andrevna3993@gmail.com>
2022-11-10 11:33:46 +0300

    Added function request in api-ingredients

--- src/utils/api-ingredients.js
+++ src/utils/api-ingredients.js
@@ -4,13 +4,16 @@ const checkResponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
 
-
-
-export const getIngredients = () => {
-  return fetch(url)
+function request(url, options) {
+  // принимает два аргумента: урл и объект опций, как и `fetch`
+  return fetch(url, options)
     .then(checkResponse)
     .then((data) => {
       if (data?.success) return data.data;
       return Promise.reject(data);
     });
+}
+
+export const getIngredients = () => {
+  return request(url);
 };
